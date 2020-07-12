6ad970907e43b94624d6098cf5b26541dc35c0da
Sean Landers <sean.landers@gmail.com>
2020-07-12 17:32:17 -0500

    created serial function

--- dial/dial-output.py
+++ dial/dial-output.py
@@ -1,10 +1,14 @@
 import serial
 
+def getNumber(ser):
+	line = ser.readline().decode('utf-8').rstrip()
+	number = line
+	return number
+
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyACM0', '9600', timeout=1)
 	ser.flush()
 
 	while True:
 		if ser.in_waiting > 0 :
-			line = ser.readline().decode('utf-8').rstrip()
-			print(line)
+			getNumber(ser)
